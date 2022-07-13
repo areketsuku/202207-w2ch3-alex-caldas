db9c77b3ad92b61b92b2e4776d2d53c3ce743e5d
Alex Caldas <alexcaldasaixarch@gmail.com>
2022-07-13 21:20:19 +0200

    bugfix: refactor function operation to reuce cognitive complexity on line 24

--- calculator-html/index.js
+++ calculator-html/index.js
@@ -5,7 +5,7 @@ let result = "";
 
 $(":button").click(function () {
   if (isNaN(this.value)) {
-    operation(this.value);
+    matematicOperations(this.value);
   } else if (number.length < 9) {
     number += this.value;
 
@@ -15,192 +15,215 @@ $(":button").click(function () {
   }
 });
 
-function refresh() {
+const refresh = function () {
   $("#calculation").html(ans + " " + operand);
   $("#number").html(number);
   result = "";
-}
+};
 
-function operation(i) {
+const matematicOperations = function (i) {
   switch (i) {
     case "A":
-      ans = "";
-      number = "";
-      operand = "";
-      refresh();
+      matematicAllClear();
       break;
     case "B":
-      if (!number) {
-      } else {
-        number = number.slice(0, number.length - 1);
-      }
-      if (number === "0") {
-        number = "";
-      }
-      refresh();
+      matematicBack();
       break;
     case "/":
-      if (ans || ans == 0) {
-        if (operand) {
-          if (number) {
-            calculation();
-            ans = result;
-            number = "";
-            operand = "/";
-            refresh();
-          } else {
-            operand = "/";
-            refresh();
-          }
-        } else {
-          operand = "/";
-          refresh();
-        }
-      } else {
-        if (operand) {
-          alert("OJU!! NO HI POT HAVER OPERAND SENSE ANS!");
-        } else {
-          if (number) {
-            ans = Number(number);
-            number = "";
-            operand = "/";
-            refresh();
-          } else {
-          }
-        }
-      }
+      matematicDivide();
       break;
     case "*":
-      if (ans || ans == 0) {
-        if (operand) {
-          if (number) {
-            calculation();
-            ans = result;
-            number = "";
-            operand = "*";
-            refresh();
-          } else {
-            operand = "*";
-            refresh();
-          }
-        } else {
-          operand = "*";
-          refresh();
-        }
-      } else {
-        if (operand) {
-          alert("OJU!! NO HI POT HAVER OPERAND SENSE ANS!");
-        } else {
-          if (number) {
-            ans = Number(number);
-            number = "";
-            operand = "*";
-            refresh();
-          } else {
-          }
-        }
-      }
+      matematicDivide();
       break;
     case "-":
-      if (ans || ans == 0) {
-        if (operand) {
-          if (number) {
-            calculation();
-            ans = result;
-            number = "";
-            operand = "-";
-            refresh();
-          } else {
-            operand = "-";
-            refresh();
-          }
-        } else {
-          operand = "-";
-          refresh();
-        }
-      } else {
-        if (operand) {
-          alert("OJU!! NO HI POT HAVER OPERAND SENSE ANS!");
-        } else {
-          if (number) {
-            ans = Number(number);
-            number = "";
-            operand = "-";
-            refresh();
-          } else {
-          }
-        }
-      }
+      matematicSubstract();
       break;
     case "+":
-      if (ans || ans == 0) {
-        if (operand) {
-          if (number) {
-            calculation();
-            ans = result;
-            number = "";
-            operand = "+";
-            refresh();
-          } else {
-            operand = "+";
-            refresh();
-          }
-        } else {
-          operand = "+";
-          refresh();
-        }
-      } else {
-        if (operand) {
-          alert("OJU!! NO HI POT HAVER OPERAND SENSE ANS!");
-        } else {
-          if (number) {
-            ans = Number(number);
-            number = "";
-            operand = "+";
-            refresh();
-          } else {
-          }
-        }
-      }
+      matematicAdd();
       break;
     case ".":
-      if (number.includes(".")) {
-      } else if (number) {
-        number += ".";
-      } else {
-        number = "0.";
-      }
-      refresh();
+      matematicDot();
       break;
     case "=":
-      if (ans || ans == 0) {
-        if (operand) {
-          if (number) {
-            calculation();
-            $("#calculation").html(ans + " " + operand + " " + number + " =");
-            $("#number").html(result);
-          } else {
-          }
-        } else {
-        }
-      } else {
-        if (operand) {
-          alert("OJU!! NO HI POT HAVER OPERAND SENSE ANS!");
-        } else {
-          if (number) {
-            ans = number;
-            number = "";
-            refresh();
-          } else {
-          }
-        }
-      }
+      matematicEqual();
       break;
     default:
   }
-}
-
-function calculation() {
+};
+const matematicAllClear = function () {
+  ans = "";
+  number = "";
+  operand = "";
+  refresh();
+};
+const matematicBack = function () {
+  if (!number) {
+  } else {
+    number = number.slice(0, number.length - 1);
+  }
+  if (number === "0") {
+    number = "";
+  }
+  refresh();
+};
+const matematicDivide = function () {
+  if (ans || ans == 0) {
+    if (operand) {
+      if (number) {
+        calculation();
+        ans = result;
+        number = "";
+        operand = "/";
+        refresh();
+      } else {
+        operand = "/";
+        refresh();
+      }
+    } else {
+      operand = "/";
+      refresh();
+    }
+  } else {
+    if (operand) {
+      alert("OJU!! NO HI POT HAVER OPERAND SENSE ANS!");
+    } else {
+      if (number) {
+        ans = Number(number);
+        number = "";
+        operand = "/";
+        refresh();
+      } else {
+      }
+    }
+  }
+};
+const matematicMultiply = function () {
+  if (ans || ans == 0) {
+    if (operand) {
+      if (number) {
+        calculation();
+        ans = result;
+        number = "";
+        operand = "*";
+        refresh();
+      } else {
+        operand = "*";
+        refresh();
+      }
+    } else {
+      operand = "*";
+      refresh();
+    }
+  } else {
+    if (operand) {
+      alert("OJU!! NO HI POT HAVER OPERAND SENSE ANS!");
+    } else {
+      if (number) {
+        ans = Number(number);
+        number = "";
+        operand = "*";
+        refresh();
+      } else {
+      }
+    }
+  }
+};
+const matematicSubstract = function () {
+  if (ans || ans == 0) {
+    if (operand) {
+      if (number) {
+        calculation();
+        ans = result;
+        number = "";
+        operand = "-";
+        refresh();
+      } else {
+        operand = "-";
+        refresh();
+      }
+    } else {
+      operand = "-";
+      refresh();
+    }
+  } else {
+    if (operand) {
+      alert("OJU!! NO HI POT HAVER OPERAND SENSE ANS!");
+    } else {
+      if (number) {
+        ans = Number(number);
+        number = "";
+        operand = "-";
+        refresh();
+      } else {
+      }
+    }
+  }
+};
+const matematicAdd = function () {
+  if (ans || ans == 0) {
+    if (operand) {
+      if (number) {
+        calculation();
+        ans = result;
+        number = "";
+        operand = "+";
+        refresh();
+      } else {
+        operand = "+";
+        refresh();
+      }
+    } else {
+      operand = "+";
+      refresh();
+    }
+  } else {
+    if (operand) {
+      alert("OJU!! NO HI POT HAVER OPERAND SENSE ANS!");
+    } else {
+      if (number) {
+        ans = Number(number);
+        number = "";
+        operand = "+";
+        refresh();
+      } else {
+      }
+    }
+  }
+};
+const matematicDot = function () {
+  if (number.includes(".")) {
+  } else if (number) {
+    number += ".";
+  } else {
+    number = "0.";
+  }
+  refresh();
+};
+const matematicEqual = function () {
+  if (ans || ans == 0) {
+    if (operand) {
+      if (number) {
+        calculation();
+        $("#calculation").html(ans + " " + operand + " " + number + " =");
+        $("#number").html(result);
+      } else {
+      }
+    } else {
+    }
+  } else {
+    if (operand) {
+      alert("OJU!! NO HI POT HAVER OPERAND SENSE ANS!");
+    } else {
+      if (number) {
+        ans = number;
+        number = "";
+        refresh();
+      } else {
+      }
+    }
+  }
+};
+const calculation = function () {
   switch (operand) {
     case "+":
       result = Number(ans) + Number(number);
@@ -215,4 +238,4 @@ function calculation() {
       result = Number(ans) / Number(number);
       break;
   }
-}
+};
